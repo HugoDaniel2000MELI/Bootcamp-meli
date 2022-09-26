cc45e63ede328c270dcbdecf354a7df03822e3c6
HugoDaniel2000MELI <hugo.caxias@mercadolivre.com>
2022-09-25 22:03:35 -0300

    add exercises

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import Pessoas.Pessoa;
+
+
+    public class Main {
+        public static void main(String[] args) {
+            Pessoa pessoa = new Pessoa();
+            Pessoa pessoa1 = new Pessoa("Hugo", 22, "M5222");
+            Pessoa pessoa2 = new Pessoa("Hugo", 22, "M7222", 70.5, 1.80);
+
+            if(pessoa2.calcularIMC() == 1){
+                System.out.println(pessoa2.getNome() + " esta com sobrepeso");
+            } else if (pessoa2.calcularIMC() == -1) {
+                System.out.println(pessoa2.getNome() + " esta abaixo do peso");
+            }else {
+                System.out.println(pessoa2.getNome() + " esta no peso ideal");
+            }
+
+        }
     }
-}
---
+++ src/Pessoas/Pessoa.java
@@ -0,0 +1,60 @@
+package Pessoas;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    String id;
+    double peso;
+    double altura;
+
+    public Pessoa(){}
+
+    public  Pessoa(String nome, int idade, String id){
+        this.nome = nome;
+        this.idade = idade;
+        this.id = id;
+    }
+
+    public  Pessoa(String nome, int idade, String id, double peso, double altura){
+        this.nome = nome;
+        this.idade = idade;
+        this.id = id;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int calcularIMC(){
+        double imc = this.peso/(Math.pow(this.altura,2));
+        if(imc >= 20 || imc <= 25){
+            return 0;
+        } else if (imc < 20) {
+            return -1;
+        }else {
+            return 1;
+        }
+    }
+
+    public boolean ehMaiorIdade(){
+        return this.idade >= 18;
+    }
+}
